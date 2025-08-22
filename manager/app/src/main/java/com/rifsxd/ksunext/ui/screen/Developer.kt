8c51b4732363515bdabba92bc9a146a39d7614f4
TheWildJames <bins4us@hotmail.com>
2025-08-22 01:52:14 -0400

    Fix missing fillMaxWidth import in Developer.kt

--- manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/Developer.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/Developer.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
